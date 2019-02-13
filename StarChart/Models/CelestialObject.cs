b943bd46953f32463151f3b714c474e1fd66ae9d
usmanamd <usmanamd@gmail.com>
2019-02-13 23:18:55 +0000

    Corrected the name

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -12,7 +12,7 @@ namespace StarChart.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        public int? OrbitObject { get; set; }
+        public int? OrbitedObject { get; set; }
         [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
