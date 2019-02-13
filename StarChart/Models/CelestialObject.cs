959ac555ab0c95f983336a63a51e27d187d35335
usmanamd <usmanamd@gmail.com>
2019-02-13 23:17:19 +0000

    Attribute added.

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +13,7 @@ namespace StarChart.Models
         [Required]
         public string Name { get; set; }
         public int? OrbitObject { get; set; }
+        [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
     }
